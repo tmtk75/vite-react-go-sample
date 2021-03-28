6d1bbba3f5244cb7f21e4b6accf64a1d93590492
Tomotaka Sakuma <ktmtmks@gmail.com>
2021-03-29 06:46:29 +0900

    new: replace with vite-react

--- src/components/UserProfile.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <div>{{ $route.params.name }}'s profile</div>
-  <div></div>
-</template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,7 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import styleImport from "vite-plugin-style-import";
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    styleImport({
-      libs: [
-        {
-          libraryName: "element-plus",
-          resolveStyle: (name: string) => {
-            name = name.substring(3);
-            return `element-plus/packages/theme-chalk/src/${name}.scss`;
-          },
-          resolveComponent: (name) => {
-            return `element-plus/lib/${name}`;
-          },
-        },
-      ],
-    }),
-  ],
-});
+  plugins: [reactRefresh()]
+})
